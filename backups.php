ca5f9d7d7f27f336d864e64410baa2b6794f7c1d
BackupGuard <BackupGuard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 12:04:13 +0000

    Readme edited.
    git-svn-id: https://plugins.svn.wordpress.org/backup-guard/tags/1.0.1@1190754 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ backups.php
@@ -0,0 +1,113 @@
+<?php
+require_once(dirname(__FILE__).'/boot.php');
+require_once(SG_PUBLIC_INCLUDE_PATH.'/header.php');
+require_once(SG_BACKUP_PATH.'SGBackup.php');
+$backups = SGBackup::getAllBackups();
+$downloadUrl = admin_url('admin-post.php?action=backup_guard_downloadBackup&');
+?>
+<?php require_once(SG_PUBLIC_INCLUDE_PATH.'sidebar.php'); ?>
+<?php if(SGConfig::get('SG_REVIEW_POPUP_STATE') == SG_SHOW_REVIEW_POPUP): ?>
+    <a href="javascript:void(0)" id="sg-review" class="hidden" data-toggle="modal" data-modal-name="manual-review" data-remote="modalReview"></a>
+    <script type="text/javascript">sgShowReview = 1;</script>
+<?php endif; ?>
+<div id="sg-content-wrapper">
+    <div class="container-fluid">
+        <fieldset>
+            <legend><?php echo _t('Backups')?></legend>
+            <a href="javascript:void(0)" id="sg-manual-backup" class="pull-left btn btn-success" data-toggle="modal" data-modal-name="manual-backup" data-remote="modalManualBackup"><i class="glyphicon glyphicon-play"></i> <?php _t('Perform manual backup')?></a>
+            <a href="javascript:void(0)" id="sg-import" class="pull-right btn btn-primary" data-toggle="modal" data-modal-name="import"  data-remote="modalImport"><i class="glyphicon glyphicon-open"></i> <?php _t('Import')?></a>
+            <div class="clearfix"></div><br/>
+            <table class="table table-striped paginated sg-backup-table">
+                <thead>
+                <tr>
+                    <th><?php _t('Filename')?></th>
+                    <th><?php _t('Size')?></th>
+                    <th><?php _t('Date')?></th>
+                    <th><?php _t('Status')?></th>
+                    <th><?php _t('Actions')?></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if(empty($backups)):?>
+                    <tr>
+                        <td colspan="5"><?php _t('No backups found.')?></td>
+                    </tr>
+                <?php endif;?>
+                <?php foreach($backups as $backup): ?>
+                    <tr>
+                        <td><?php echo $backup['name'] ?></td>
+                        <td><?php echo !$backup['active']?$backup['size']:'' ?></td>
+                        <td><?php echo $backup['date'] ?></td>
+                        <td id="sg-status-tabe-data" <?php echo $backup['active']?'data-toggle="tooltip" data-placement="top" data-original-title="" data-container="#sg-wrapper"':''?>>
+                            <?php if($backup['active']):
+                                $activeOptions = json_decode(SGConfig::get('SG_ACTIVE_BACKUP_OPTIONS'), true);
+                                $filteredStatuses = filterStatusesByActionType($backup, $activeOptions);
+                                ?>
+                                <input type="hidden" id="sg-active-action-id" value="<?php echo $backup['id']?>"/>
+                                <?php foreach ($filteredStatuses as $statusCode): ?>
+                                <span class="btn-xs sg-status-icon sg-status-<?php echo $statusCode ?>">&nbsp;</span>
+                            <?php endforeach; ?>
+                                <div class="progress sg-progress">
+                                    <div class="progress-bar progress-bar-danger"></div>
+                                </div>
+                            <?php else: ?>
+                                <?php if ($backup['backup_warning'] == 1 || $backup['restore_warning'] == 1): ?>
+                                    <span class="glyphicon glyphicon-warning-sign btn-xs text-warning" data-toggle="tooltip" data-placement="top" data-original-title="<?php echo $backup['backup_warning']?_t('Warnings found during backup',true):_t('Warnings found during restore',true)?>" data-container="#sg-wrapper"></span>
+                                <?php elseif ($backup['backup_error'] == 1 || $backup['restore_error'] == 1): ?>
+                                    <span class="glyphicon glyphicon-warning-sign btn-xs text-danger" data-toggle="tooltip" data-placement="top" data-original-title="<?php echo $backup['backup_error']?_t('Errors found during backup',true):_t('Errors found during restore',true)?>" data-container="#sg-wrapper"></span>
+                                <?php else: ?>
+                                    <span class="glyphicon glyphicon-ok btn-xs text-success"></span>
+                                <?php endif;?>
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <?php if($backup['active']): ?>
+                                <a class="btn btn-danger btn-xs sg-cancel-backup" href="javascript:void(0)" title="<?php _t('Stop')?>">&nbsp;<i class="glyphicon glyphicon-stop" aria-hidden="true"></i>&nbsp;</a>
+                            <?php else: ?>
+                                <a href="javascript:void(0)" data-sgbackup-name="<?php echo $backup['name'];?>" data-remote="deleteBackup" class="btn btn-danger btn-xs sg-remove-backup" title="<?php _t('Delete')?>">&nbsp;<i class="glyphicon glyphicon-remove" aria-hidden="true"></i>&nbsp;</a>
+                                <div class="btn-group">
+                                    <a href="javascript:void(0)" class="btn btn-primary dropdown-toggle btn-xs" data-toggle="dropdown" aria-expanded="false" title="<?php _t('Download')?>">
+                                        &nbsp;<i class="glyphicon glyphicon-download-alt" aria-hidden="true"></i>&nbsp;
+                                        <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <?php if($backup['files']):?>
+                                            <li>
+                                                <a href="<?php echo $downloadUrl.'backupName='.$backup['name'].'&downloadType='.SG_BACKUP_DWONLOAD_TYPE_SGBP ?>">
+                                                    <i class="glyphicon glyphicon-hdd" aria-hidden="true"></i> <?php _t('Backup')?>
+                                                </a>
+                                            </li>
+                                        <?php endif;?>
+                                        <?php if($backup['backup_log']):?>
+                                            <li>
+                                                <a href="<?php echo $downloadUrl.'backupName='.$backup['name'].'&downloadType='.SG_BACKUP_DWONLOAD_TYPE_BACKUP_LOG ?>">
+                                                    <i class="glyphicon glyphicon-list-alt" aria-hidden="true"></i> <?php _t('Backup log')?>
+                                                </a>
+                                            </li>
+                                        <?php endif;?>
+                                        <?php if($backup['restore_log']):?>
+                                            <li>
+                                                <a href="<?php echo $downloadUrl.'backupName='.$backup['name'].'&downloadType='.SG_BACKUP_DWONLOAD_TYPE_RESTORE_LOG ?>">
+                                                    <i class="glyphicon glyphicon-th-list" aria-hidden="true"></i> <?php _t('Restore log')?>
+                                                </a>
+                                            </li>
+                                        <?php endif;?>
+                                    </ul>
+                                </div>
+                                <a href="javascript:void(0)" title="<?php _t('Restore')?>" class="btn btn-success btn-xs sg-restore" data-restore-name="<?php echo $backup['name']?>">
+                                    &nbsp;<i class="glyphicon glyphicon-repeat" aria-hidden="true"></i>&nbsp;
+                                </a>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+            <div class="text-right">
+                <ul class="pagination"></ul>
+            </div>
+        </fieldset>
+    </div>
+    <?php require_once(SG_PUBLIC_INCLUDE_PATH.'/footer.php'); ?>
+</div>
+<div class="clearfix"></div>
---
+++ js/main.js
@@ -0,0 +1,156 @@
+sgBackup = {};
+sgBackup.isModalOpen = false;
+SG_CURRENT_ACTIVE_AJAX = '';
+
+jQuery(window).load(function() {
+    sgBackup.showReviewModal();
+});
+
+jQuery(document).ready( function() {
+    sgBackup.init();
+});
+
+//SG init
+sgBackup.init = function(){
+    sgBackup.initModals();
+};
+
+//SG Modal popup logic
+sgBackup.initModals = function(){
+    jQuery('[data-toggle="modal"][href], [data-toggle="modal"][data-remote]').off('click').on('click', function(e) {
+        e.preventDefault();
+        var btn = jQuery(this),
+            url = btn.attr('data-remote'),
+            modalName = btn.attr('data-modal-name'),
+            modal = jQuery('#sg-modal');
+        if( modal.length == 0 ) {
+            modal = jQuery('' +
+            '<div class="modal fade" id="sg-modal" tabindex="-1" role="dialog" aria-hidden="true"></div>' +
+            '');
+            body.append(modal);
+        }
+        sgBackup.showAjaxSpinner('#sg-content-wrapper');
+
+        var ajaxHandler = new sgRequestHandler(url, {});
+        ajaxHandler.type = 'GET';
+        ajaxHandler.dataType = 'html';
+        ajaxHandler.callback = function(data, error) {
+            sgBackup.hideAjaxSpinner();
+            if (error===false) {
+                jQuery('#sg-modal').append(data);
+            }
+            modal.on('hide.bs.modal', function() {
+                if(SG_CURRENT_ACTIVE_AJAX != '') {
+                    if (!confirm('Are you sure you want to cancel?')) {
+                        return false;
+                    }
+                    SG_CURRENT_ACTIVE_AJAX.abort();
+                    SG_CURRENT_ACTIVE_AJAX = '';
+                }
+            });
+            modal.one('hidden.bs.modal', function() {
+                modal.html('');
+            }).modal('show');
+            sgBackup.didOpenModal(modalName);
+        };
+        ajaxHandler.run();
+    });
+};
+
+sgBackup.didOpenModal = function(modalName){
+    if(modalName == 'manual-backup'){
+        sgBackup.initManulBackupRadioInputs();
+        sgBackup.initManualBackupTooltips();
+    }
+    else if(modalName == 'import'){
+        sgBackup.initFileUpload();
+    }
+    else if(modalName == 'ftp-settings'){
+        jQuery('#sg-modal').on('hidden.bs.modal', function () {
+            if(sgBackup.isFtpConnected != true) {
+                jQuery('input[data-storage=ftp]').bootstrapSwitch('state', false);
+            }
+        })
+    }
+    else if(modalName == 'manual-review'){
+        var action = 'setReviewPopupState';
+        jQuery('#sgLeaveReview').click(function(){
+            var reviewUrl = jQuery(this).attr('data-review-url');
+            //Never show again
+            var reviewState = 2;
+            var ajaxHandler = new sgRequestHandler(action, {reviewState: reviewState});
+            ajaxHandler.run();
+            window.open(reviewUrl);
+        });
+
+        jQuery('#sgDontAskAgain').click(function(){
+            //Never show again
+            var reviewState = 2;
+            var ajaxHandler = new sgRequestHandler(action, {reviewState: reviewState});
+            ajaxHandler.run();
+        });
+
+        jQuery('#sgAskLater').click(function(){
+            var reviewState = 0;
+            var ajaxHandler = new sgRequestHandler(action, {reviewState: reviewState});
+            ajaxHandler.run();
+        });
+    }
+};
+
+sgBackup.isAnyOpenModal = function(){
+    return jQuery('#sg-modal').length;
+};
+
+sgBackup.alertGenerator = function(content, alertClass){
+    var sgalert = '';
+    sgalert+='<div class="alert alert-dismissible '+alertClass+'">';
+    sgalert+='<button type="button" class="close" data-dismiss="alert">×</button>';
+    if(jQuery.isArray(content)){
+        jQuery.each(content, function(index, value) {
+            sgalert+=value+'<br/>';
+        });
+    }
+    else if(content != ''){
+        sgalert+=content.replace('[','').replace(']','').replace('"','');
+    }
+    sgalert+='</div>';
+    return sgalert;
+};
+
+sgBackup.scrollToElement = function(id){
+    if(jQuery(id).position()){
+        if(jQuery(id).position().top < jQuery(window).scrollTop()){
+            //scroll up
+            jQuery('html,body').animate({scrollTop:jQuery(id).position().top}, 1000);
+        }
+        else if(jQuery(id).position().top + jQuery(id).height() > jQuery(window).scrollTop() + (window.innerHeight || document.documentElement.clientHeight)){
+            //scroll down
+            jQuery('html,body').animate({scrollTop:jQuery(id).position().top - (window.innerHeight || document.documentElement.clientHeight) + jQuery(id).height() + 15}, 1000);
+        }
+    }
+};
+
+sgBackup.showAjaxSpinner = function(appendToElement){
+    if(typeof appendToElement == 'undefined'){
+        appendToElement = '#sg-wrapper';
+    }
+    jQuery('<div class="sg-spinner"></div>').appendTo(appendToElement);
+};
+
+sgBackup.hideAjaxSpinner = function(){
+    jQuery('.sg-spinner').remove();
+};
+
+less.pageLoadFinished.then(
+    function() {
+        jQuery('#sg-wrapper').show();
+        jQuery('.sg-spinner').remove();
+    }
+);
+
+sgBackup.showReviewModal = function(){
+    if(typeof sgShowReview != 'undefined') {
+        jQuery('#sg-review').trigger("click");
+    }
+};
